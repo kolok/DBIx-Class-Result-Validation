270ca1e6fcc01ace54f3ffd9abb24ab2dac451e2
Nicolas Oudard <noudard@weborama.com>
2011-02-09 18:59:10 +0100

    change version and changes

--- lib/DBIx/Class/Result/Validation.pm
+++ lib/DBIx/Class/Result/Validation.pm
@@ -9,12 +9,12 @@ DBIx::Class::Result::Validation - DBIx::Class component to manage validation on 
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
