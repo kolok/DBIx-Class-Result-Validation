84f0444283025db3c6d2050f7d708790d785cd14
Nicolas Oudard <noudard@weborama.com>
2017-04-21 09:58:51 +0200

    Bump to version 0.14

--- lib/DBIx/Class/Result/Validation.pm
+++ lib/DBIx/Class/Result/Validation.pm
@@ -14,11 +14,11 @@ DBIx::Class::Result::Validation - DBIx::Class component to manage validation on 
 
 =head1 VERSION
 
-Version 0.13
+Version 0.14
 
 =cut
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 
 =head1 SYNOPSIS
 
